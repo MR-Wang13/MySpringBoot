181ef998e61c633d58317fc96d5164cd33378077
Yishan Wang <wangyishan0313@qq.com>
2023-12-27 13:02:00 -0800

    capacitor

---
+++ src/main/java/com/example/myspringboot/config/CorsConfig.java
@@ -0,0 +1,27 @@
+package com.example.myspringboot.config;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public FilterRegistrationBean corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("http://localhost:8100");
+        config.addAllowedOrigin("capacitor://localhost");
+        config.addAllowedOrigin("http://localhost");
+        config.addAllowedOrigin("http://localhost:80");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", config);
+        FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
+        bean.setOrder(0);        return bean;
+    }
+}
